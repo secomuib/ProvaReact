5e391e3a99d5ca923d10512bdb40c105c1a035d4
maciamut <maciamut@users.noreply.github.com>
2021-07-14 11:04:19 +0200

    Update2
    Update2

--- setters_and_getters.sol
+++ setters_and_getters.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.5.7;
-contract Transfer_Asset_Ownership {
+contract stters_and_getters {
 
 address public owner;
 address public contractAddress; 
-bytes public sha256hash; 
+//bytes public sha256hash;
+uint public myNewNumber;
 
 constructor() public payable {
     owner = msg.sender;
@@ -15,7 +16,11 @@ function changeOwner(address payable _owner) public {
     owner = _owner;
 }
 
-function setsha256hash(bytes memory _sha256hash) public {
-    sha256hash = _sha256hash;
+function addNumber(uint _addThisNumber) public {
+    myNewNumber = _addThisNumber + myNewNumber;
+}
+
+function addTenToMyNumber() public view returns (uint) {
+    return myNewNumber + 10;
 }
 }
